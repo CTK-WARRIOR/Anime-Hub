b02d8d125c296776f49dee29d0997f63f5063c43
CTK WARRIOR <57840201+CTK-WARRIOR@users.noreply.github.com>
2021-08-22 10:36:51 +0530

    Update recentAnime.js

--- cache/recentAnime.js
+++ cache/recentAnime.js
@@ -1,11 +1,9 @@
-const Anime = require("ctk-anime-scraper")
-
-module.exports = (cache) => {
-setInterval(async () => {
-  let scrapedRecent = await Anime.getRecentAnime()
-  if(JSON.stringify(scrapedRecent) !== JSON.stringify(cache.recentAnime) && scrapedRecent.length) {
-    cache.recentAnime = scrapedRecent
-    console.log({action: "Updated the Recent Anime!"})
-  }
-}, 60000)
+module.exports = (cache, Anime) => {
+  setInterval(async () => {
+    let scrapedRecent = await Anime.getRecentAnime()
+    if (JSON.stringify(scrapedRecent) !== JSON.stringify(cache.recentAnime) && scrapedRecent.length) {
+      cache.recentAnime = scrapedRecent
+      console.log({ action: "Updated the Recent Anime!" })
+    }
+  }, 60000)
 }
